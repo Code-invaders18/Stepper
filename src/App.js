3df4ad6433b5fa6da110c2e6ba2c69a109503bf6
sourodeep23 <98368233+sourodeep23@users.noreply.github.com>
2022-08-02 11:13:32 +0530

    stepper component complete

--- src/App.js
+++ src/App.js
@@ -3,45 +3,46 @@ import "./App.css";
 import { useState } from "react";
 
 function Stepper() {
-    const [currentState, setCurrentState] = useState(0)
+    const [currentState, setCurrentState] = useState(1)
     const [totalState, setTotalState] = useState([
         {
-            name:"",
-            index:0,
-            description:""
+            name: "",
+            index: 0,
+            description: ""
         },
         {
-            name:"",
-            index:0,
-            description:""
-        },{
-            name:"",
-            index:0,
-            description:""
-        },{
-            name:"",
-            index:0,
-            description:""
-        },{
-            name:"",
-            index:0,
-            description:""
+            name: "",
+            index: 1,
+            description: ""
+        }, {
+            name: "",
+            index: 2,
+            description: ""
+        }, {
+            name: "",
+            index: 3,
+            description: ""
+        }, {
+            name: "",
+            index: 4,
+            description: ""
         }
     ])
-  return (
-    <div class="c-stepper">
+    return (
+        <div class="c-stepper">
 
-        {totalState.map(state=>{
-            return <div  class="c-stepper__item">
-                <div className="c-stepper__item__content">
-                    1
+            {totalState.map(state => {
+                return <div className={`${state.index >= currentState ? 'c-stepper__item' : 'c-stepper__item__selected'}`}>
+                    <div className={`${state.index > currentState ? 'c-stepper__item__content' : 'c-stepper__item__content__selected'}`}>
+                        {state.index == currentState && state.index + 1}
+                        {state.index < currentState && <ion-icon name="checkmark"></ion-icon>}
+                    </div>
                 </div>
-            </div>
-        })}
-      {/* <div class="c-stepper__item__selected">
+            })}
+            {/* <div class="c-stepper__item__selected">
       </div> */}
-    </div>
-  );
+        </div>
+    );
 }
 
 export default Stepper;
